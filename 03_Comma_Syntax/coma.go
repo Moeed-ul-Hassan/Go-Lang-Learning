67a0c232031e69f0d3e49bb111c9d64758bcd580
Your Name <moeedmughal682@gmail.com>
2025-12-27 15:03:33 +0500

    remove unused files and add new implementation for reading user input

---
+++ 03_Comma_Syntax/coma.go
@@ -0,0 +1,16 @@
+package main
+
+// := is an walrus operator
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin) //stdin means standard input. It is a way to read input from the user via the terminal or command line.
+	fmt.Println("Enter text:")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered:", input)
+
+}
