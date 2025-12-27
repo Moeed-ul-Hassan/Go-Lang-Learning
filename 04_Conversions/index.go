83ec3a5d38a88cb6123008b933bb19b26d3f2783
Your Name <moeedmughal682@gmail.com>
2025-12-27 15:45:25 +0500

    add initial implementation of Pizza App with user input for pizza size

---
+++ 04_Conversions/index.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to our Pizza App..!") // Corrected typo: "Welcome"
+	fmt.Println("Please enter the pizza size you want to order (e.g., 12 for 12 inches):")
+	reader := bufio.NewReader(os.Stdin)
+	sizeInput, _ := reader.ReadString('\n')
+	trimmedSizeInput := strings.TrimSpace(sizeInput)
+	pizzaSize, err := strconv.ParseFloat(trimmedSizeInput, 64)
+	if err != nil {
+		fmt.Println("Invalid input. Please enter a numeric value for the pizza size.")
+		return
+	}
+
+	fmt.Printf("Thanks for Ordering! You have entered %.2f inches pizza size.\n", pizzaSize)
+}
